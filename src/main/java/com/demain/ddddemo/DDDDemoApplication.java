648162e85b01e655c84ded86fccb39952d58f7d7
demain_lee <lmm_work@163.com>
2022-10-26 16:59:28 +0800

    init commit

---
+++ src/main/java/com/demain/ddddemo/DDDDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demain.ddddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDDDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DDDDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demain/ddddemo/DDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demain.ddddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
